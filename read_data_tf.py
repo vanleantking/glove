6c4e8f9d03037a9382f36432ff645c719b9138b9
vanleantking <van.le.k3c1@gmail.com>
2017-11-09 04:38:41 +0700

    post clustering in hierachy

--- read_data_tf.py
+++ read_data_tf.py
@@ -1,25 +1,31 @@
+import numpy as np
+import tensorflow as tf
+from collections import Counter, defaultdict
 import tf_glove
 import re
-import nltk
-from collections import Counter, defaultdict
 import os
 import sys
+import pickle
 import bs4 as bs
 import unicodedata
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import cophenet
+from scipy.cluster.hierarchy import fcluster
+from scipy.spatial.distance import pdist
 from string import punctuation
+import nltk
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-from scipy.spatial.distance import cdist
-from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
+from nltk.stem.snowball import SnowballStemmer
+from scipy.cluster.hierarchy import dendrogram, linkage
 from bikmeans import BiKMeans
 from KMedoids import KMedoids
 from sklearn.cluster import KMeans
+from scipy.spatial.distance import cdist
+from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.metrics.pairwise import pairwise_distances
-from nltk.stem.snowball import SnowballStemmer
-import tensorflow as tf
-import pickle
+from nameprocessing import PostClusterProcessing
 # import kmedoids
 from owlready2 import *
 
@@ -53,29 +59,33 @@ def load_corpus():
                 for sent in sents:
                     if sent.strip():                        
                         
-                        #remove date in sentence
-                        sentence = "".join(word for word in sent if not re.search(r'[0-9]{1,4}[\/,:,-][0-9]{1,3}([\/,:,-][0-9]{2,4})?([\/,:,-][0-9]{2,4})?', word))
+                        words_arrays = re.findall(r"[\w']+|[.,!?;\/+]", sent.strip().lower())
 
-                        sentence = "".join(word for word in sentence if not re.search(r'\'s', word))
+                        #remove date in sentence
+                        sentence = [word for word in words_arrays if not re.search(r'[0-9]{1,4}[\/,:,-][0-9]{1,3}([\/,:,-][0-9]{2,4})?([\/,:,-][0-9]{2,4})?', word)]
+
+                        # sentence = [word for word in sentence if not re.search(r'\'s', word)]
 
                         #remove specials symbols
-                        sentence = "".join(c for c in sentence if c not in ('!','.',':', '-', '+', '_', '(', ')', '*', '&', '#', ';', '?', '>', '<', '%', '{', '}', '=', ',', ']', '[', '`', '\''))
+                        sentence = [c for c in sentence if c not in ('!','.',':', '-', '+', '_', '(', ')', '*', '&', '#', ';', '?', '>', '<', '%', '{', '}', '=', ',', ']', '[', '`', '\'')]
 
-                        sentence = "".join(c for c in sentence if not re.search(r'^/[/]?', c))
-
+                        sentence = [c for c in sentence if not re.search(r'^/[/]?', c)]
                         #remove digits in sentence
-                        sentence = "".join(word for word in sentence if not re.search(r'^\d+(mg)?', word))
+                        sentence = [word for word in sentence if not re.search(r'\d+(\.\s?\d+)?\w*', word)]
+                        sentence = [c for c in sentence if not re.search(r'^mg', c)]
+                        
+
+                        sentence = [c for c in sentence if not re.search(r'``|\'\'', c)]
 
                         #remove stopwords in sentence
-                        process_words = " ".join(word for word in sentence.split() if word not in stopwords.words('english'))
+                        # process_words = [word for word in sentence if word not in stopwords.words('english')]
 
                         #stemming words in sentences
-                        process_words = "".join(stemmer.stem(t) for t in process_words)
+                        # process_words = [stemmer.stem(t) for t in process_words]
 
                         #tokenizer sentence
-                        words_arrays = word_tokenize(process_words.strip().lower())
-                        words_train += len(words_arrays)
-                        texts.append(words_arrays)
+                        words_train += len(sentence)
+                        texts.append(sentence)
     return texts, words_train
 
 def find_ngrams(input_list, n):
@@ -116,6 +126,7 @@ def learning_phrase(corpus, corpus_size, word_phrases = 4, min_count = 2, thresh
                     del ngram_counter[line[i]+"_"+line[i+1]]
                 i = i + 1
         corpuses.append(text)
+    file.close()
     corpus_dict['corpus'] = corpuses
     corpus_dict['corpus_size'] = corpus_size
     pickle.dump( corpus_dict, open( "tf_corpus.p", "wb" ) )
@@ -142,14 +153,36 @@ def load_data():
                     if line.strip():
                         yield nltk.wordpunct_tokenize(line.strip())
     
+def fancy_dendrogram(*args, **kwargs):
+    max_d = kwargs.pop('max_d', None)
+    if max_d and 'color_threshold' not in kwargs:
+        kwargs['color_threshold'] = max_d
+    annotate_above = kwargs.pop('annotate_above', 0)
 
+    ddata = dendrogram(*args, **kwargs)
+
+    if not kwargs.get('no_plot', False):
+        plt.title('Hierarchical Clustering Dendrogram (truncated)')
+        plt.xlabel('sample index or (cluster size)')
+        plt.ylabel('distance')
+        for i, d, c in zip(ddata['icoord'], ddata['dcoord'], ddata['color_list']):
+            x = 0.5 * sum(i[1:3])
+            y = d[1]
+            if y > annotate_above:
+                plt.plot(x, y, 'o', c=c)
+                plt.annotate("%.3g" % y, (x, y), xytext=(0, -5),
+                             textcoords='offset points',
+                             va='top', ha='center')
+        if max_d:
+            plt.axhline(y=max_d, c='k')
+    return ddata
 
     
 if __name__ == '__main__':
     
     onto = get_ontology("file:///media/vanle/Studying/python/readOntology/newemr.owl").load()
     embedding = None
-
+    docsclustering = open("doctorcluster.txt","w")
     if os.path.isfile(corpus_file):
         corpus_dict = pickle.load( open( corpus_file, "rb" ) )
         corpus = corpus_dict['corpus']
@@ -158,11 +191,13 @@ if __name__ == '__main__':
         corpus, corpus_size = load_corpus()
         corpus = learning_phrase(corpus, corpus_size)
     print('learning phrase completed')
-    file = open("learning_phrase.txt","w")
+    file = open("corpus.txt","w")
     for text in corpus:
         file.write(' '.join(text))
         file.write('\n')
     file.close()
+
+    print('log corpus completed')
 
 
     model = tf_glove.GloVeModel(embedding_size=100, context_size=1)
@@ -210,8 +245,8 @@ if __name__ == '__main__':
             doctorRecord = []
             # , professionRecord, citieRecord, streetRecord, stateRecord, hospitalRecord, organizationRecord, countrieRecord 
             doctorRecord = [doctor.hasName for doctor in medicalRecord.doctor_dianose]
-            doctorRecord = [model.embeded_phrases(doctor.hasName) for doctor in medicalRecord.doctor_dianose]
-            doctorIndex.extend([{"name": doctor, "value": model.embeded_phrases(doctor.hasName)} for doctor in medicalRecord.doctor_dianose])
+            doctorRecord = [model.embeded_phrases(doctor.hasName[0]) for doctor in medicalRecord.doctor_dianose]
+            doctorIndex.extend([{"name": doctor, "value": model.embeded_phrases(doctor.hasName[0])} for doctor in medicalRecord.doctor_dianose])
             # professionRecord = [model.embeded_phrases(profession.jobName) for profession in medicalRecord.job_position]
             # citieRecord = [model.embeded_phrases(city.hasLocation) for city in medicalRecord.has_city]
             # streetRecord = [model.embeded_phrases(street.hasLocation) for street in medicalRecord.has_street]
@@ -224,15 +259,72 @@ if __name__ == '__main__':
         # print(doctors)
         # D = pairwise_distances(doctors, metric='cosine')        
 
-        docs = KMeans(n_clusters=4, random_state=0).fit(doctors)
-        print(docs.labels_)
-        print(doctorIndex)
-        # docscluster = {i : [] for i in range(4)}
-        # for index, label in enumerate(docs.labels_):
-        #     docscluster[label].append(docIndex['name'] for docIndex in doctorIndex if np.array_equal(docIndex['value'],doctors[index]))
+
+        #KMeans clustering
+        # docs = KMeans(n_clusters=4, random_state=0).fit(doctors)
+        # print("KMean clustering result---------------------------------------------")
+        # print(docs.labels_)
+        
+        
         # print(docscluster)
-        D = 1 - cdist(doctors, doctors, 'cosine')
-        print(D, doctors)
+        # print(len(doctors))
+        try:
+            Z = linkage(doctors, 'single', 'cosine')
+        except:
+            print(num, doctors)
+            print(doctorIndex)
+            # return
+        fig = plt.figure(figsize=(25, 10))
+        dn = dendrogram(Z)
+        c, coph_dists = cophenet(Z, pdist(doctors, metric='cosine'))
+        # print(c)
+        # print(Z)
+        clusters = fcluster(Z, 0.5, criterion='distance')
+        clusters_number =  len(np.unique(clusters))
+        print(clusters_number)
+        docscluster = {i : [] for i in range(1, clusters_number+1)}
+        # for index, label in enumerate(clusters):
+        for index, docIndex in enumerate(doctorIndex):
+            if np.array_equal(docIndex['value'],doctors[index]):
+                docscluster[clusters[index]].append(docIndex['name'] )
+
+        docsclustering.write("result patienttttttttttttttttttttttttttttttttttttttttttttttttt: " + str(patientRecord.hasRecordName[0]))
+        
+        # print(clusters)
+        pcp = PostClusterProcessing()
+        for i in range(1, clusters_number+1):
+            for j in range (i+1, clusters_number+1):
+                if i in docscluster.keys() and j in docscluster.keys():
+                    merged = pcp.mergecluster(docscluster[i], docscluster[j])
+                    if merged:
+                        try:
+                            docscluster[i].extend(docscluster[j])
+                            del docscluster[j]
+                        except:
+                            pass
+
+        docsclustering.write('\n')
+        for k, v in docscluster.items():
+            docsclustering.write('cluster ' + str(k))
+            docsclustering.write(''.join(str(v)))
+            docsclustering.write('\n')
+        docsclustering.write('\n\n\n\n\n')
+        # for doc in docscluster:
+        #     print(doc.values())
+
+        # print(fancy_dendrogram(
+        #     Z,
+        #     truncate_mode='lastp',
+        #     p=12,
+        #     leaf_rotation=90.,
+        #     leaf_font_size=12.,
+        #     show_contracted=True,
+        #     annotate_above=10,
+        #     max_d=0.5,  # plot a horizontal cut-off line
+        # ))
+        # plt.show()
+        # D = 1 - cdist(doctors, doctors, 'cosine')
+        # print(D, doctors)
         # M, C = kmedoids.kMedoids(D, 4)
         # print('')
         # print('clustering result:')
@@ -245,9 +337,10 @@ if __name__ == '__main__':
         #     docscluster[label].append(doctors[index])
         #     states = 
         #     medicalRecord.was_use_device
-        break
+        # if (num == 10):
+        #     break
 
-
+    docsclustering.close()
     # patitens = []
     # for i in onto.Patient.instances():
     #     patitens.append(model.embeded_phrases(i.name))
@@ -263,26 +356,23 @@ if __name__ == '__main__':
 
     # docs = KMeans(n_clusters=10, random_state=0).fit(l_doctors)
     # print(cosine_similarity([model.embeded_phrases("Yechiel Kidd")], [model2.embeded_phrases("Kidd")]))
-    print(cosine_similarity([model.embeded_phrases("Yechiel Kidd")], [model.embeded_phrases("Kidd")]))
-    print(cosine_similarity([model.embeded_phrases("Y Ullrich")], [model.embeded_phrases("Y. Ullrich")]))
-    print(cosine_similarity([model.embeded_phrases("Y Ullrich")], [model.embeded_phrases("Ullrich")]))
-    print(cosine_similarity([model.embeded_phrases("Yosef Q Ullrich")], [model.embeded_phrases("Ullrich")]))
-    print(cosine_similarity([model.embeded_phrases("Y Ullrich")], [model.embeded_phrases("Y. Ullrich")]))
-    print(cosine_similarity([model.embeded_phrases("Ullysses B. Gilbert")], [model.embeded_phrases("Ullysses Gilbert")]))
-    print(cosine_similarity([model.embeded_phrases("Ullysses B. Gilbert")], [model.embeded_phrases("Ullysses Gilbert")]))
-    print(cosine_similarity([model.embeded_phrases("Ullysses B. Gilbert")], [model.embeded_phrases("Gilbert")]))
-    print(cosine_similarity([model.embeded_phrases("Gilbert")], [model.embeded_phrases("Ullysses Gilbert")]))
-    print(cosine_similarity([model.embeded_phrases("Yosef Q Ullrich")], [model.embeded_phrases("Herman N. Weller")]))
+    # print(cosine_similarity([model.embeded_phrases("Yechiel Kidd")], [model.embeded_phrases("Kidd")]))
+    # print(cosine_similarity([model.embeded_phrases("Y Ullrich")], [model.embeded_phrases("Y. Ullrich")]))
+    # print(cosine_similarity([model.embeded_phrases("Y Ullrich")], [model.embeded_phrases("Ullrich")]))
+    # print(cosine_similarity([model.embeded_phrases("Yosef Q Ullrich")], [model.embeded_phrases("Ullrich")]))
+    # print(cosine_similarity([model.embeded_phrases("Y Ullrich")], [model.embeded_phrases("Y. Ullrich")]))
+    # print(cosine_similarity([model.embeded_phrases("Ullysses B. Gilbert")], [model.embeded_phrases("Ullysses Gilbert")]))
+    # print(cosine_similarity([model.embeded_phrases("Ullysses B. Gilbert")], [model.embeded_phrases("Ullysses Gilbert")]))
+    # print(cosine_similarity([model.embeded_phrases("Ullysses B. Gilbert")], [model.embeded_phrases("Gilbert")]))
+    # print(cosine_similarity([model.embeded_phrases("Gilbert")], [model.embeded_phrases("Ullysses Gilbert")]))
+    # print(cosine_similarity([model.embeded_phrases("Yosef Q Ullrich")], [model.embeded_phrases("Herman N. Weller")]))
     # print(cosine_similarity(l_doctors))
     # print(model.embeded_phrases("Betty Kaitlin Wood"))
 
     # print(doctors)
     # print(docs.labels_)
-    print("Betty Kaitlin Wood: ", cosine_similarity([model.embeded_phrases("Betty Kaitlin Wood")], l_doctors))
-    print("Herman N. Weller: ", cosine_similarity([model.embeded_phrases("Herman N. Weller")], l_doctors))
-    print("Yosef Q Ullrich: ", cosine_similarity([model.embeded_phrases("Yosef Q Ullrich")], l_doctors))
     
-    model.generate_tsne(path='log/tsne')
+    # model.generate_tsne(path='log/tsne')
 
 
     
--- tf_glove.py
+++ tf_glove.py
@@ -5,6 +5,7 @@ from random import shuffle
 import tensorflow as tf
 from nltk.util import ngrams
 import numpy as np
+import re
 
 
 class NotTrainedError(Exception):
@@ -177,8 +178,10 @@ class GloVeModel():
     
     
     def embeded_phrases(self, phrases):
-        phrases = "".join(c for c in phrases if c not in ('!','.',':', '-', '\'s', '+', '_', '(', ')', '*', '&', '#', ';', '?', '>', '<', '%'))
-        words = phrases.strip().lower().split()
+        # phrases = "".join(c for c in phrases if c not in ('!','.',':', '-', '\'s', '+', '_', '(', ')', '*', '&', '#', ';', '?', '>', '<', '%'))
+        phrases = re.findall(r"[\w']+|[.,!?;\/+]", phrases.strip().lower())
+        words = [c for c in phrases if c not in ('!','.',':', '-', '+', '_', '(', ')', '*', '&', '#', ';', '?', '>', '<', '%', '{', '}', '=', ',', ']', '[', '`', '\'')]
+        # words = sentence.strip().lower().split()
         phrases = '_'.join(words)
         try:
             vec = self.embedding_for(phrases)
@@ -204,7 +207,7 @@ class GloVeModel():
         if tf.train.latest_checkpoint(save_dir) is not None:
             with tf.Session() as session:
                 ckpt = tf.train.get_checkpoint_state(save_dir)
-                saver = tf.train.import_meta_graph('tf_glove.model-94050.meta')
+                saver = tf.train.import_meta_graph('tf_glove.model-95700.meta')
                 saver.restore(session, ckpt.model_checkpoint_path)
                 graph = tf.get_default_graph()
                 embedding_object = graph.get_tensor_by_name('combined_embeddings:0')
